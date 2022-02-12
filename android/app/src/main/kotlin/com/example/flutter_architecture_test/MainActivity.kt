e6c3586481b14de6ad911cd4003bf34235682b36
zazila1 <zazila1@gmail.com>
2022-02-12 21:52:50 +0300

    wip

--- android/app/src/main/kotlin/com/example/flutter_architecture_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_architecture_test/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.new_app
+package com.example.flutter_architecture_test
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ packages/architecture_test_business/lib/src/models/hotels_notifier.dart
@@ -0,0 +1,12 @@
+import 'package:architecture_test_business/architecture_test_business.dart';
+import 'package:flutter/material.dart';
+
+abstract class HotelsNotifier with ChangeNotifier  {
+  //late bool isLoaded;
+  late Future<List<HotelPreview>> previewHotelData;
+  late Future<Hotel> hotelData;
+
+  void loadHotelsPreviewData();
+  void loadHotelData(String uuid, {bool notify});
+  void updateHotel();
+}
