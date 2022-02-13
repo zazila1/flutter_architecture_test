75eeaea628e7833b7dfccecc9dbd36b049f7156d
zazila1 <zazila1@gmail.com>
2022-02-13 04:10:23 +0300

    wip

--- packages/architecture_test_data/lib/src/api/api.dart
+++ packages/architecture_test_data/lib/src/api/api.dart
@@ -2,7 +2,6 @@ import 'package:architecture_test_data/src/models/hotel_response.dart';
 import 'package:architecture_test_data/src/models/hotels_preview_response.dart';
 
 abstract class Api<T> {
-  //Api(T _httpClient, [String? _baseUrl = ""]);
   Future<List<HotelPreviewResponse>> getHotelsPreviewData();
   Future<HotelResponse> getHotelData(String uuid);
 }
